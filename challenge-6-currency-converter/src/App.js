549d8214982a88c8caf91888050e5df70ab3e369
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-19 17:48:44 +0800

    trying to get output

--- challenge-6-currency-converter/src/App.js
+++ challenge-6-currency-converter/src/App.js
@@ -1,19 +1,45 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useState, useEffect } from "react";
+
 export default function App() {
+  const [currencyFrom, setCurrencyFrom] = useState("USD");
+  const [currencyTo, setCurrencyTo] = useState("USD");
+  const [amount, setAmount] = useState("23");
+
+  useEffect(
+    function () {
+      async function convertExchange() {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${currencyFrom}&to=${currencyTo}`
+        );
+        const data = res.json();
+        console.log(data);
+      }
+      convertExchange();
+    },
+    [amount, currencyFrom, currencyTo]
+  );
+
   return (
     <div>
-      <Amount />
-      <Currency />
-      <Currency />
+      <Amount amount={amount} setAmount={setAmount} />
+      <Currency
+        currencySelect={currencyFrom}
+        setCurrencySelect={setCurrencyFrom}
+      />
+      <Currency currencySelect={currencyTo} setCurrencySelect={setCurrencyTo} />
       <Output />
     </div>
   );
 }
 
-function Currency() {
+function Currency({ currencySelect, setCurrencySelect }) {
   return (
-    <select>
+    <select
+      value={currencySelect}
+      onChange={(e) => setCurrencySelect(e.target.value)}
+    >
       <option value="USD">USD</option>
       <option value="EUR">EUR</option>
       <option value="CAD">CAD</option>
@@ -26,6 +52,12 @@ function Output() {
   return <p>output</p>;
 }
 
-function Amount() {
-  return <input type="text" />;
+function Amount({ amount, setAmount }) {
+  return (
+    <input
+      type="text"
+      value={amount}
+      onChange={(e) => setAmount(e.target.value)}
+    />
+  );
 }
