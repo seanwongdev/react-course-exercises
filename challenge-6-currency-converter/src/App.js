fa921470c8c2221900ef64e3b2f1d174243b8d54
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-20 10:20:49 +0800

    disabled inputs when page is loading and when currency is same, return same amount as result

--- challenge-6-currency-converter/src/App.js
+++ challenge-6-currency-converter/src/App.js
@@ -7,27 +7,29 @@ export default function App() {
   const [currencyTo, setCurrencyTo] = useState("USD");
   const [amount, setAmount] = useState("23");
   const [result, setResult] = useState("");
+  const [loading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       async function convertExchange() {
         try {
+          setIsLoading(true);
           const res = await fetch(
             `https://api.frankfurter.app/latest?amount=${amount}&from=${currencyFrom}&to=${currencyTo}`
           );
           console.log(res);
-          if (!res.ok)
-            throw new Error(
-              "Currency To should be different from Currency From"
-            );
+          if (!res.ok) throw new Error("Fetching went wrong");
           const data = await res.json();
 
           console.log(data);
           setResult(data.rates[currencyTo]);
         } catch (err) {
           setResult(err.message);
+        } finally {
+          setIsLoading(false);
         }
       }
+      if (currencyFrom === currencyTo) return setResult(amount);
       convertExchange();
     },
     [amount, currencyFrom, currencyTo]
@@ -35,13 +37,13 @@ export default function App() {
 
   return (
     <div>
-      <Amount amount={amount} setAmount={setAmount} />
+      <Amount amount={amount} setAmount={setAmount} loading={loading} />
       <Currency
         currencySelect={currencyFrom}
         setCurrencySelect={setCurrencyFrom}
       />
       <Currency currencySelect={currencyTo} setCurrencySelect={setCurrencyTo} />
-      <Output result={result} />
+      <Output result={result} currencyTo={currencyTo} />
     </div>
   );
 }
@@ -60,16 +62,21 @@ function Currency({ currencySelect, setCurrencySelect }) {
   );
 }
 
-function Output({ result }) {
-  return <p>{result}</p>;
+function Output({ result, currencyTo }) {
+  return (
+    <p>
+      {result} {currencyTo}
+    </p>
+  );
 }
 
-function Amount({ amount, setAmount }) {
+function Amount({ amount, setAmount, loading }) {
   return (
     <input
       type="text"
       value={amount}
       onChange={(e) => setAmount(e.target.value)}
+      disabled={loading}
     />
   );
 }
