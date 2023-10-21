839e50b648e607c90a67a498a1e4db258a35215f
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-21 22:20:16 +0800

    added changes

--- challenge-7-usegeolocate/src/App.js
+++ challenge-7-usegeolocate/src/App.js
@@ -1,18 +1,11 @@
 import { useState } from "react";
 
-function useGeolocation() {}
-
-export default function App() {
+function useGeolocation() {
   const [isLoading, setIsLoading] = useState(false);
-  const [countClicks, setCountClicks] = useState(0);
   const [position, setPosition] = useState({});
   const [error, setError] = useState(null);
 
-  const { lat, lng } = position;
-
   function getPosition() {
-    setCountClicks((count) => count + 1);
-
     if (!navigator.geolocation)
       return setError("Your browser does not support geolocation");
 
@@ -31,10 +24,27 @@ export default function App() {
       }
     );
   }
+  return { isLoading, position, error, getPosition };
+}
+
+export default function App() {
+  const [countClicks, setCountClicks] = useState(0);
+
+  const {
+    isLoading,
+    position: { lat, lng },
+    error,
+    getPosition,
+  } = useGeolocation();
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
 
   return (
     <div>
-      <button onClick={getPosition} disabled={isLoading}>
+      <button onClick={handleClick} disabled={isLoading}>
         Get my position
       </button>
 
