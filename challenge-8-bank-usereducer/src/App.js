4cd8e4c551d01c9a6c4467c9bc0b8430280393e3
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-22 10:25:49 +0800

    done!

--- challenge-8-bank-usereducer/src/App.js
+++ challenge-8-bank-usereducer/src/App.js
@@ -1,47 +1,29 @@
 import { useReducer } from "react";
 import "./styles.css";
 
-/*
-INSTRUCTIONS / CONSIDERATIONS:
-
-1. Let's implement a simple bank account! It's similar to the example that I used as an analogy to explain how useReducer works, but it's simplified (we're not using account numbers here)
-
-2. Use a reducer to model the following state transitions: openAccount, deposit, withdraw, requestLoan, payLoan, closeAccount. Use the `initialState` below to get started.
-
-3. All operations (expect for opening account) can only be performed if isActive is true. If it's not, just return the original state object. You can check this right at the beginning of the reducer
-
-4. When the account is opened, isActive is set to true. There is also a minimum deposit amount of 500 to open an account (which means that the balance will start at 500)
-
-5. Customer can only request a loan if there is no loan yet. If that condition is met, the requested amount will be registered in the 'loan' state, and it will be added to the balance. If the condition is not met, just return the current state
-
-6. When the customer pays the loan, the opposite happens: the money is taken from the balance, and the 'loan' will get back to 0. This can lead to negative balances, but that's no problem, because the customer can't close their account now (see next point)
-
-7. Customer can only close an account if there is no loan, AND if the balance is zero. If this condition is not met, just return the state. If the condition is met, the account is deactivated and all money is withdrawn. The account basically gets back to the initial state
-*/
-
 const initialState = {
   balance: 0,
   loan: 0,
   isActive: false,
 };
 
-const DEPOSIT_FIXED = 150;
-const WITHDRAW_FIXED = 50;
 const LOAN = 5000;
 
 function reducer(state, action) {
+  if (!state.isActive && action.type !== "openAccount") return state;
+
   switch (action.type) {
     case "openAccount":
       return { ...state, isActive: true, balance: 500 };
     case "deposit":
-      return { ...state, balance: state.balance + DEPOSIT_FIXED };
+      return { ...state, balance: state.balance + action.payload };
     case "withdraw":
-      return { ...state, balance: state.balance - WITHDRAW_FIXED };
+      return { ...state, balance: state.balance - action.payload };
     case "requestLoan":
       return {
         ...state,
-        balance: state.loan ? state.balance : state.balance + LOAN,
-        loan: state.loan ? state.loan : LOAN,
+        balance: state.loan ? state.balance : state.balance + action.payload,
+        loan: state.loan ? state.loan : action.payload,
       };
     case "payLoan":
       return {
@@ -81,7 +63,7 @@ export default function App() {
       </p>
       <p>
         <button
-          onClick={() => dispatch({ type: "deposit" })}
+          onClick={() => dispatch({ type: "deposit", payload: 150 })}
           disabled={!isActive}
         >
           Deposit 150
@@ -89,7 +71,7 @@ export default function App() {
       </p>
       <p>
         <button
-          onClick={() => dispatch({ type: "withdraw" })}
+          onClick={() => dispatch({ type: "withdraw", payload: 50 })}
           disabled={!isActive}
         >
           Withdraw 50
@@ -98,7 +80,7 @@ export default function App() {
       <p>
         <button
           onClick={() => {
-            dispatch({ type: "requestLoan" });
+            dispatch({ type: "requestLoan", payload: 5000 });
           }}
           disabled={!isActive}
         >
