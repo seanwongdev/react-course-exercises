1729b66b544e09ed7649da9741776e5a0d361641
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-27 09:45:36 +0800

    added data-loader, errorElement, useNavigation

--- challenge-10-fast-react-pizza/src/App.jsx
+++ challenge-10-fast-react-pizza/src/App.jsx
@@ -1,5 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import CreateUser from "./features/user/CreateUser";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      { path: "/order/:orderId", element: <Order /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div>hello vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
