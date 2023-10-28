3b01b25a20e6b48ed5f63f349ad6dca3108e2548
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2023-10-28 09:41:12 +0800

    styled cart

--- challenge-10-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ challenge-10-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -41,27 +41,32 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="post">
-        <div>
-          <label>First Name</label>
-          <input type="text" className="input" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:place-items-center ">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" className="input grow" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:place-items-center ">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:place-items-center ">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
--- challenge-10-fast-react-pizza/src/features/user/CreateUser.jsx
+++ challenge-10-fast-react-pizza/src/features/user/CreateUser.jsx
@@ -24,7 +24,7 @@ function CreateUser() {
 
       {username !== "" && (
         <div>
-          <Button type="primary"=>Start ordering</Button>
+          <Button type="primary">Start ordering</Button>
         </div>
       )}
     </form>
